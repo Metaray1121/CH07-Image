cf98ce30ea4e99f18ec5e453555db5b03f7ac9d5
Metaray1121 <metaray1121@gmail.com>
2021-12-02 16:40:37 +0800

    Initial commit

---
+++ 0700_LifeCycle/src/main/java/tw/tcnr12/m0700/M0700.java
@@ -0,0 +1,91 @@
+package tw.tcnr12.m0700;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class M0700 extends AppCompatActivity {
+
+    private  final  String TAG = "tcnr12=>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m0700);
+        Log.d(TAG,"onCreate");
+        setupViewComponent();
+    }
+
+    private void setupViewComponent() {
+        int a = 9999 + 123;
+        Log.d(TAG,"setupViewComponent()" + a);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.m0700, menu);
+        return true;
+    }
+//
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        Toast.makeText(getApplication(),"禁用返回鍵",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                this.finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
